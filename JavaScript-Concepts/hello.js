72f2b8cac082b65f74d76f5948b2edbd416d844b
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-02-09 19:06:05 +0530

    Js first code

---
+++ JavaScript-Concepts/hello.js
@@ -0,0 +1,18 @@
+function printChai () {
+    console.log("Hello chai");
+
+}
+function bringBrush(noOfBrush) {
+    // console.log("Hanji le aye brush" , itne);
+       console.log(`Hanji le aye ${noOfBrush} brush `)
+}
+function addTwo(num1 ,num2) {
+ return num1 +num2 
+}
+
+
+
+bringBrush(4);
+printChai ();
+addTwo(1,1);
+console.log(addTwo(2 , 2))
