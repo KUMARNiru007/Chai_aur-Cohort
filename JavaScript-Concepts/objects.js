20082ff0389cb2213b5e33a93586ac6a9dd7c109
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-02-13 18:55:48 +0530

    objects

---
+++ JavaScript-Concepts/objects.js
@@ -0,0 +1,27 @@
+const person ={
+    x:10,
+    firstName:'Kumar',
+    lastName : 'Nirupam',
+    nobbies:['Coding','Gym'],
+    getfullNmae: function(){
+        return 'Kumar'
+    },
+    address:{
+        hno:1,
+        street:2,
+
+    }
+}
+console.log(person.address.hno)
+console.log(person)
+
+const remote={
+    color:'blue',
+    brand:'xyz',
+    turnOff:function(){
+        
+    },
+    volumeUp: function(){
+        
+    }
+}
