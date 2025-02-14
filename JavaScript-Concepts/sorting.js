f97b313d65b4e2f09e6ea8223e02221c36bd6273
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-02-14 10:15:40 +0530

    learn sorting and revseer the array

---
+++ JavaScript-Concepts/sorting.js
@@ -0,0 +1,14 @@
+const arr=[1,2,1000,55,7]
+console.log(arr.sort())
+
+//[1,1000,2,55,7]
+
+// sort function short the unit code values in ascending order and not the whole value.
+
+//sort(compareFn?: (a: T, b: T) => number): this;
+    /**
+     * Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+     * @param start The zero-based location in the array from which to start removing elements.
+     * @param deleteCount The number of elements to remove.
+     * @returns An array containing the elements that were deleted.
+     */
