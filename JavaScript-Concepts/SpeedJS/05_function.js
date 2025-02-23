16c789f0806084985608073f2bd1c44ee6054011
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-02-23 23:17:54 +0530

    cohort class lecture

---
+++ JavaScript-Concepts/SpeedJS/05_function.js
@@ -0,0 +1,45 @@
+function greet(name){
+    console.log(`Hello ${name}`)
+}
+
+greet ('Hitesh')
+greet("Piyush")
+//------------------------------------------------------------------------------------------------
+let globalVar  = "I am global"
+
+function modifyGlobal() {
+    globalVar = "I am modified"
+    let blockScopeVar = "I am blocked-scope"
+    console.log(blockScopeVar)
+
+}
+modifyGlobal()
+
+//------------------------------------------------------------------------------------------------
+
+let config = function() {
+    let settings= {
+        theme: "dark"
+    }
+    return settings
+}  () // function create and called here iffe 
+// one time execute
+(() => {}) ()
+
+
+
+//------------------------------------------------------------------------------------------------
+let person1 ={
+    personsName : "ravi",
+    greet: function() {
+        console.log(`Hello, ${this.personName}`)
+
+    }
+}
+
+let person2= {
+    personName:'kumar'
+}
+
+person1.greet.call({personName: Mukul}) // bz we need to change
+// call() use when we need to change contect
