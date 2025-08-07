e86a9ec1ea08772c990062b6fd595bb2b4ef67fb
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-08-07 19:29:15 +0530

    react codes

--- React/07react/src/App.jsx
+++ React/07react/src/App.jsx
@@ -1,9 +1,11 @@
 import {useState , useEffect} from "react";
+import { ChaiMenu } from "./AllChai";
+import { useSpecialChai } from "./hooks/useSpecialChai.js";
 
 
 export function App (){
-
-  const [message , setMessage] = useState("Loading...")  // [var , func] comming ans return of function useState
+  const { chai,loading, error } = useSpecialChai()
+  const [message , setMessage] = useState(1)  // [var , func] comming ans return of function useState
 
   let anotherMessage ; // not good to use let 
 
@@ -18,13 +20,18 @@ export function App (){
     .catch(() => setMessage("Failed to load"))
 
    // return () // unMount --> function to next page or clean up to next 
-  }, [] // dependency arry  --  bar bar call karna bar bar change -- wiil trigger when there will be some changes in the work 
-  )
+  }, [] )// dependency arry  --  bar bar call karna bar bar change -- wiil trigger when there will be some changes in the work 
+
+ if(loading) return <h2>Loading...</h2>
+ if(Error)  return <h2>Error  </h2>
+
   return (
     <div>
         <h1>Welcome to chaicode</h1>
         <p>Serving hot chai with react</p>
         <h2>{message} </h2>
+        <ChaiMenu />
+        <h3>{chai.name}</h3>
     </div>
   )
 }
